a9073484eef72f61e4c387578079bf28688b43ca
r4-keisuke <rijkard@gmail.com>
2018-11-26 09:36:24 +0900

    test

--- skills/interactive_messages.js
+++ skills/interactive_messages.js
@@ -3,7 +3,7 @@ module.exports = function(controller) {
     // create special handlers for certain actions in buttons
     // if the button action is 'say', act as if user said that thing
     controller.middleware.receive.use(function(bot, message, next) {
-      console.log(message);
+      console.log("received!");
       if (message.type == 'message_action') {
           console.log(message);
       }
