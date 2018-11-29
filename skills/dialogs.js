b7c97fc61905035042e2963d1e5d86e1c1941394
r4-keisuke <rijkard@gmail.com>
2018-11-29 22:59:16 +0900

    log dialog

--- skills/dialogs.js
+++ skills/dialogs.js
@@ -17,8 +17,29 @@ module.exports = function(controller) {
     // the values from the form are in event.submission    
     controller.on('dialog_submission', function (bot, message) {
         var submission = message.submission;
+        console.log(message);
         bot.reply(message, 'Got it!');
-
+        /*
+        request({
+            url: "http://${github_user_id}@${process.env.github_access_token}:api.github.com/repos/:${github_user_id}/:${github_reponame}/issues",
+            method: "POST",
+            json: true,
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent": "UA(必須)"
+            },
+            form: {
+                "title": "fuga"
+            }
+            }
+        }, (err, res) => {
+            if (err) {
+                console.log(err);
+                return false;
+            }
+            return res;
+        });
+        */
         // call dialogOk or else Slack will think this is an error
         bot.dialogOk();
     });
